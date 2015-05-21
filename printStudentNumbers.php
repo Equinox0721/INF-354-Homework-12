3ada1629ff3589b477e926bca7f14a8422df584a
u13027922 <jaycevaneck@gmail.com>
2015-05-21 09:06:20 +0200

    Update printStudentNumbers.php

--- printStudentNumbers.php
+++ printStudentNumbers.php
@@ -3,7 +3,7 @@
 	  echo("<marquee behavior = 'scroll direction = 'left' scrollamount = '17'><span style = 'font-size: 18pt'>Saskia Kuschke - u13025148</span></marquee>");	
 	  echo("<br />");
 	  echo("<br />");
-	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount= '12'><span style = 'font-size: 30pt'>Jason van Eck - u13027922</span></marquee>");
+	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount= '12'><span style = 'font-size: 24pt'>Jason van Eck - u13027922</span></marquee>");
 	  echo("<br />");
 	  echo("<br />");
 	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '15'><span style = 'font-size: 18pt'>Ferdinand Groenewald - u13046447</span></marquee>");
