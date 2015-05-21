ea6540a295dbfce4c35a679374b05cc693d4439d
Equinox0721 <saskia.kuschke@aliquantumtrust.com>
2015-05-21 09:01:23 +0200

    Added my line of code
    Again.

--- printStudentNumbers.php
+++ printStudentNumbers.php
@@ -3,7 +3,7 @@
 	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount= '10'><span style = 'font-size: 30pt'>Jason van Eck - u13027922</span></marquee>");
 	  echo("<br />");
 	  echo("<br />");
-	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '25'><span style = 'font-size: 16pt'>YOUR NAME AND STUDENT NUMBER HERE</span></marquee>");
+	  echo("<marquee behavior = 'scroll direction = 'left' scrollamount = '15'><span style = 'font-size: 12pt'>Saskia Kuschke - u13025148</span></marquee>");	
 	  echo("<br />");
 	  echo("<br />");
 	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '15'><span style = 'font-size: 12pt'>Ferdinand Groenewald - u13046447</span></marquee>");
