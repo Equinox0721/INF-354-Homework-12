129fdc36eb6c0b21d1f4d89af085a69a6ce9b956
Xenelith <bosrotwow@yahoo.co.uk>
2015-05-21 08:52:56 +0200

    Update printStudentNumbers.php

--- printStudentNumbers.php
+++ printStudentNumbers.php
@@ -1,6 +1,6 @@
 <?php
   echo("<marquee behavior='scroll' direction='left' scrollamount='5'>Jason van Eck - u13027922</marquee>");
   echo("<marquee behavior='scroll' direction='left' scrollamount='10'>Saskia Kuschke - u13025148</marquee>");
-  echo("<marquee behavior='scroll' direction='left' scrollamount='15'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
+  echo("<marquee behavior='scroll' direction='left' scrollamount='15'>Ferdinand Groenewald - u13046447</marquee>");
   echo("<marquee behavior='scroll' direction='left' scrollamount='20'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
 ?>
