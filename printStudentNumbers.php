9b0a56578c659a677f5dd54265e60c8afcc4dabf
u13027922 <jaycevaneck@gmail.com>
2015-05-21 08:48:37 +0200

    Update printStudentNumbers.php

--- printStudentNumbers.php
+++ printStudentNumbers.php
@@ -1,3 +1,6 @@
 <?php
-  echo("Jason van Eck - u13027922");
+  echo("<marquee behavior='scroll' direction='left' scrollamount='5'>Jason van Eck - u13027922</marquee>");
+  echo("<marquee behavior='scroll' direction='left' scrollamount='10'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
+  echo("<marquee behavior='scroll' direction='left' scrollamount='15'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
+  echo("<marquee behavior='scroll' direction='left' scrollamount='20'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
 ?>
