f2be7e57090acf845290d14a2e3b08d7819d4b92
u13027922 <jaycevaneck@gmail.com>
2015-05-21 08:56:27 +0200

    Update printStudentNumbers.php

--- printStudentNumbers.php
+++ printStudentNumbers.php
@@ -1,6 +1,14 @@
 <?php
-  echo("<marquee behavior='scroll' direction='left' scrollamount='5'>Jason van Eck - u13027922</marquee>");
-  echo("<marquee behavior='scroll' direction='left' scrollamount='10'>Saskia Kuschke - u13025148</marquee>");
-  echo("<marquee behavior='scroll' direction='left' scrollamount='15'>Ferdinand Groenewald - u13046447</marquee>");
-  echo("<marquee behavior='scroll' direction='left' scrollamount='20'>YOUR NAME AND STUDENT NUMBER HERE</marquee>");
+  echo("<div style = 'font-family: Calibri'>");
+	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount= '10'><span style = 'font-size: 30pt'>Jason van Eck - u13027922</span></marquee>");
+	  echo("<br />");
+	  echo("<br />");
+	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '25'><span style = 'font-size: 16pt'>YOUR NAME AND STUDENT NUMBER HERE</span></marquee>");
+	  echo("<br />");
+	  echo("<br />");
+	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '15'><span style = 'font-size: 12pt'>YOUR NAME AND STUDENT NUMBER HERE</span></marquee>");
+	  echo("<br />");
+	  echo("<br />");
+	  echo("<marquee behavior = 'scroll' direction = 'left' scrollamount = '20'><span style = 'font-size: 20pt'>YOUR NAME AND STUDENT NUMBER HERE</span></marquee>");
+  echo("</div>");
 ?>
